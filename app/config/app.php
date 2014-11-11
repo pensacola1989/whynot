9b27691e4c9dac5b9bdd3e7328d4ede1d21f05a6
www <qindongwoxin@gmail.com>
2014-11-11 17:43:24 +0800

    add ACL

--- app/config/app.php
+++ app/config/app.php
@@ -122,6 +122,7 @@ return array(
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
 		'McCool\LaravelAutoPresenter\LaravelAutoPresenterServiceProvider',
+		'Zizaco\Entrust\EntrustServiceProvider',
 		'Way\Generators\GeneratorsServiceProvider'
 	),
 
@@ -189,7 +190,7 @@ return array(
 		'URL'             => 'Illuminate\Support\Facades\URL',
 		'Validator'       => 'Illuminate\Support\Facades\Validator',
 		'View'            => 'Illuminate\Support\Facades\View',
-
+		'Entrust' 		  => 'Zizaco\Entrust\EntrustFacade'
 	),
 
 );
